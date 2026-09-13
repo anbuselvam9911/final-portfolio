22bbd1ae470ba1f8139a0665418b6501a512437f
Anbu Selvam <anbuselvam9911@mountzion.ac.in>
2026-09-13 08:40:03 +0530

    Delete motion.js

--- motion.js
+++
@@ -1,1 +0,0 @@
-window.revealOnScroll=function(){const items=document.querySelectorAll('.reveal');const observer=new IntersectionObserver(entries=>{entries.forEach(entry=>{if(entry.isIntersecting){entry.target.classList.add('visible');observer.unobserve(entry.target)}})},{threshold:.14});items.forEach(item=>observer.observe(item))};
